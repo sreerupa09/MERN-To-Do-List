09df90a8e840bf9b0a325f2b904c120a67aef5a3
Sreerupa Banik <117094225+sreerupa09@users.noreply.github.com>
2024-02-17 19:32:30 +0530

    Added Update component

--- frontend/src/components/todo/Todo.jsx
+++ frontend/src/components/todo/Todo.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
 import './Todo.css'
 import TodoCards from './TodoCards';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Update from './Update';
 
 const Todo = () => {
     const [inputs, setInputs]=useState({title:'',body:''});
@@ -13,47 +16,73 @@ const Todo = () => {
         setInputs({...inputs,[name]:value});
     };
     const submit = () => {
-        setArray([...array,inputs]);
-        setInputs({title:'',body:''});
+        if(inputs.title ==='' || inputs.body === '')
+        {
+            toast.error('Title Or Body Should Not Be Empty');
+        }
+        else
+        {
+            setArray([...array,inputs]);
+            setInputs({title:'',body:''});
+            toast.success('Your Task is Added');
+            toast.error('Your Task is Not Saved! Please Sign Up');
+        }
     };
+    const del = (id) => {
+        array.splice(id,"1");
+        setArray([...array]);
+    }
+    const dis=(value) => {
+        console.log(value);
+        document.getElementById('todo-update').style.display=value;
+    }
+
   return (
-    <div className='todo'>
-        <div className='todo-main container d-flex justify-content-center align-items-center my-4 flex-column'>
-            <div className='d-flex flex-column todo-inputs-div w-50 p-1'>
-                <input
-                type='text'
-                placeholder='Title'
-                className='my-2 p-2 todo-inputs'
-                name='title'
-                value={inputs.title}
-                onClick={show}
-                onChange={change}
-                />
-                <textarea id='textarea'
-                type='text'
-                placeholder='Body'
-                className=' p-2 todo-inputs'
-                name='body'
-                value={inputs.body}
-                onChange={change}
-                />
+    <>
+        <div className='todo'>
+            <ToastContainer />
+            <div className='todo-main container d-flex justify-content-center align-items-center my-4 flex-column'>
+                <div className='d-flex flex-column todo-inputs-div w-50 p-1'>
+                    <input
+                    type='text'
+                    placeholder='Title'
+                    className='my-2 p-2 todo-inputs'
+                    name='title'
+                    value={inputs.title}
+                    onClick={show}
+                    onChange={change}
+                    />
+                    <textarea id='textarea'
+                    type='text'
+                    placeholder='Body'
+                    className=' p-2 todo-inputs'
+                    name='body'
+                    value={inputs.body}
+                    onChange={change}
+                    />
+                </div>
+                <div className='w-50 d-flex justify-content-end my-3'>
+                    <button className='home-btn px-2 py-1' onClick={submit}>Add</button>
+                </div>
             </div>
-            <div className='w-50 d-flex justify-content-end my-3'>
-                <button className='home-btn px-2 py-1' onClick={submit}>Add</button>
-            </div>
-        </div>
-        <div className='todo-body'>
-            <div className='container-fluid'>
-                <div className='row'>
-                    {array && array.map((item,index)=>(
-                    <div className='col-lg-3 col-10 mx-5 my-2'>
-                        <TodoCards title={item.title} body={item.body} />
+            <div className='todo-body'>
+                <div className='container-fluid'>
+                    <div className='row'>
+                        {array && array.map((item,index)=>(
+                        <div className='col-lg-3 col-10 mx-5 my-2' key={index}>
+                            <TodoCards title={item.title} body={item.body} id={index} delid={del} display={dis}/>
+                        </div>
+                    ))}
                     </div>
-                ))}
                 </div>
             </div>
         </div>
-    </div>
+        <div className='todo-update' id='todo-update'>
+            <div className='container update'>
+                <Update display={dis} />
+            </div>
+        </div>
+    </>
   )
 }
 
--- frontend/src/components/todo/TodoCards.jsx
+++ frontend/src/components/todo/TodoCards.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { AiFillDelete } from "react-icons/ai";
 import { GrUpdate } from "react-icons/gr";
 
-const TodoCards = ({ title, body }) => {
+const TodoCards = ({ title, body, id, delid, display }) => {
   return (
     <div className="p-3 todo-card">
       <div>
@@ -10,10 +10,18 @@ const TodoCards = ({ title, body }) => {
         <p className="todo-card-p">{body.slice("", 77)}...</p>
       </div>
       <div className="d-flex justify-content-around">
-        <div className="d-flex justify-content-center align-items-center card-icon-head px-2 py-1 card-text">
+        <div className="d-flex justify-content-center align-items-center card-icon-head px-2 py-1 card-text"
+        onClick={() => {
+            display('block');
+        }}>
           <GrUpdate className="card-icons update" /> Update
         </div>
-        <div className="d-flex justify-content-center align-items-center card-icon-head px-2 py-1 card-text">
+        <div
+          className="d-flex justify-content-center align-items-center card-icon-head px-2 py-1 card-text"
+          onClick={() => {
+            delid(id);
+          }}
+        >
           <AiFillDelete className="card-icons del card-icon-head" /> Delete
         </div>
       </div>
