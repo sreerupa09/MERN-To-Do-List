2ec74491f3d71a1b331202b5f35dae0c944f263c
Sreerupa Banik <117094225+sreerupa09@users.noreply.github.com>
2024-02-20 20:09:02 +0530

    Fixed signin page

--- frontend/src/components/signup/Signin.jsx
+++ frontend/src/components/signup/Signin.jsx
@@ -26,46 +26,72 @@ const Signin = () => {
   //     history('/todo');
   //   });
   // }
+
+  /* NEW SUBMIT */
+  // const submit = async (e) => {
+  //   e.preventDefault();
+  //   await axios.post('http://localhost:1000/api/v1/signin',inputs,{ timeout: 5000 })
+  //     .then((res) => {
+  //       console.log(res.data.others._id);
+  //       sessionStorage.setItem("id",res.data.others._id);
+  //       dispatch(authActions.login());
+  //       history('/todo');
+  //     })
+  //     .catch((error) => {
+  //       console.error("There was an error!", error);
+  //     });
+  // }
+
   const submit = async (e) => {
     e.preventDefault();
-    await axios.post('http://localhost:1000/api/v1/signin',inputs,{ timeout: 5000 })
-      .then((res) => {
-        console.log(res.data.others._id);
+    try{
+      const res=await axios.post('http://localhost:1000/api/v1/signin',inputs);
+      console.log(res.data);
+      if(res.data)
+      {
         sessionStorage.setItem("id",res.data.others._id);
         dispatch(authActions.login());
         history('/todo');
-      })
-      .catch((error) => {
-        console.error("There was an error!", error);
-      });
+      }
+      else
+      {
+        console.error('Response data or _id not found in the response');
+      }
+    }
+    catch(error) 
+    {
+      console.error("There was an error!", error);
+    }
   }
   
   return (
-    <div className="signup">
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-4 column col-left d-none d-flex justify-content-center align-items-center">
-            <HeadingCompo first='Sign' second='In'/>
-          </div>
-          <div className="col-lg-8 column d-lg-flex justify-content-center align-items-center">
-            <div className="d-flex flex-column w-100 p-3">
-              <input
-                className="p-2 my-3 input-signup"
-                type="email"
-                name="email"
-                placeholder="Enter Your Email"
-                value={inputs.email}
-                onChange={change}
-              />
-              <input
-                className="p-2 my-3 input-signup"
-                type="password"
-                name="password"
-                placeholder="Enter Your Password"
-                value={inputs.password}
-                onChange={change}
-              />
-            <button className="btn-signup p-2" onClick={submit}>Sign In</button>
+    <div>
+      <div className="signup">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-4 column col-left d-none d-lg-flex justify-content-center align-items-center">
+              <HeadingCompo first='Sign' second='In'/>
+            </div>
+            <div className="col-lg-8 column d-flex justify-content-center align-items-center">
+              <div className="d-flex flex-column w-100 p-3">
+                <input
+                  className="p-2 my-3 input-signup"
+                  type="email"
+                  name="email"
+                  placeholder="Enter Your Email"
+                  value={inputs.email}
+                  onChange={change}
+                />
+                <input
+                  className="p-2 my-3 input-signup"
+                  type="password"
+                  name="password"
+                  placeholder="Enter Your Password"
+                  value={inputs.password}
+                  onChange={change}
+                />
+              <button className="btn-signup p-2" onClick={submit}>Sign In</button>
+              </div>
             </div>
           </div>
         </div>
